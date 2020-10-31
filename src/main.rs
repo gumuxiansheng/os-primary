9a109cde51ed415195347d9e28812cd49196bafd
Mike Zhu <zhuyuan2706@gmail.com>
2020-10-31 16:52:02 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 #![no_main]
 #![no_std]
 
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
-
-use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
