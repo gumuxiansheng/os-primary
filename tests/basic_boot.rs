bcbc5a818edeb4dc7f606b7a44c25e822586f5cd
Mike Zhu <zhuyuan2706@gmail.com>
2020-11-23 11:56:06 +0800

    testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,5 +1,5 @@
-#![no_main]
 #![no_std]
+#![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(os_primary::test_runner)]
 #![reexport_test_harness_main = "test_main"]
@@ -7,25 +7,23 @@
 use core::panic::PanicInfo;
 use os_primary::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("\nHello World{}", "!");
-    
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))] // new attribute
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     os_primary::test_panic_handler(info)
+}
+
+// Test cases
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
 }
